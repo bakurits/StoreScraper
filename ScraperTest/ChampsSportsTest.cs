3fec02444e3908da7774672499182a3719058834
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-07-19 19:08:46 +0400

    Created sample functions for ChampsSportsScraper, created ChampsSportsTest

---
+++ ScraperTest/ChampsSportsTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScraperTest
+{
+    [TestClass]
+    public class ChampsSportsTest
+    {
+        [TestMethod]
+        public void test1()
+        {
+            
+        }
+    }
+}
