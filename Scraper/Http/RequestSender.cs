6759500b779bf968b5f38355210a3f7215902a5f
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-07-22 06:19:07 +0400

    Integrated Websites

---
+++ Scraper/Http/RequestSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using StoreScraper.Factory;
+
+namespace StoreScraper.Http
+{
+    public class RequestSender
+    {
+        public HttpClient Client { get; set; }
+        public HttpClientHandler Handler { get; set; }
+    }
+}
---
+++ Scraper/Http/StringPair.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreScraper.Http
+{
+    public class StringPair
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public static implicit operator StringPair((string name, string value) pair)
+        {
+            return new StringPair()
+            {
+               Name = pair.name,
+               Value = pair.value,
+            };
+        }
+
+        public static implicit operator (string name, string value) (StringPair header)
+        {
+            return (header.Name, header.Value);
+        }
+    }
+}
