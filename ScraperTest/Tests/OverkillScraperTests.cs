c44b24eb7ca85fbe64c903d100e476808c058d11
Elie Louis <elie_louis@hotmail.com>
2018-08-08 15:13:27 +0300

    Overkill done

---
+++ ScraperTest/Tests/OverkillScraperTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreScraper.Bots.Overkill;
+using StoreScraper.Models;
+namespace ScraperTest.Tests
+{
+    [TestClass]
+    public class OverkillScraperTests
+    {
+        [TestMethod]
+        public void FindItemsTest()
+        {
+            OverkillScraper scraper = new OverkillScraper();
+            SearchSettingsBase settings = new SearchSettingsBase()
+            {
+                KeyWords = "jordan"
+            };
+
+            scraper.FindItems(out var lst, settings, CancellationToken.None);
+        }
+
+        [TestMethod()]
+        public void GetProductDetailsTest()
+        {
+            OverkillScraper scraper = new OverkillScraper();
+            Product curProduct = new Product(scraper,
+                "Whatever",
+                "https://www.overkillshop.com/en/adidas-wmns-track-top-dh4196.html",
+                1,
+                "whatever",
+                "whatever",
+                "EUR");
+
+            ProductDetails details = scraper.GetProductDetails(curProduct, CancellationToken.None);
+            foreach (var sz in details.SizesList)
+            {
+                Debug.WriteLine(sz);
+            }
+        }
+    }
+}
