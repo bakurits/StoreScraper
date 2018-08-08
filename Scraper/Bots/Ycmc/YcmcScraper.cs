fdde2881a9df1906f67f0c62ba33695d0eace688
Elie Louis <elie_louis@hotmail.com>
2018-08-08 12:58:39 +0300

    Ycmc done

---
+++ Scraper/Bots/Ycmc/YcmcScraper.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Linq;
+using HtmlAgilityPack;
+using StoreScraper.Factory;
+using StoreScraper.Helpers;
+using StoreScraper.Core;
+using StoreScraper.Models;
+using System.Text.RegularExpressions;
+using System;
+
+
+namespace StoreScraper.Bots.Ycmc
+{
+    public class YcmcScraper : ScraperBase
+    {
+        public override string WebsiteName { get; set; } = "Ycmc";
+        public override string WebsiteBaseUrl { get; set; } = "https://www.ycmc.com/";
+        public override bool Active { get; set; }
+
+        private const string SearchFormat = @"https://www.ycmc.com/new-arrivals.html?limit=120";
+        private const string priceRegex = "\\$(\\d+(\\.\\d+)?)";
+
+
+        public override void FindItems(out List<Product> listOfProducts, SearchSettingsBase settings, CancellationToken token)
+        {
+            listOfProducts = new List<Product>();
+            HtmlNodeCollection itemCollection = GetProductCollection(settings, token);
+
+            foreach (var item in itemCollection)
+            {
+                token.ThrowIfCancellationRequested();
+#if DEBUG
+                LoadSingleProduct(listOfProducts, settings, item);
+#else
+                LoadSingleProductTryCatchWraper(listOfProducts, settings, item);
+#endif
+            }
+
+        }
+
+        private void LoadSingleProductTryCatchWraper(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
+        {
+            try
+            {
+                LoadSingleProduct(listOfProducts, settings, item);
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.WriteErrorLog(e.Message);
+            }
+        }
+
+        public override ProductDetails GetProductDetails(Product product, CancellationToken token)
+        {
+            var document = GetWebpage(product.Url, token);
+            ProductDetails details = new ProductDetails();
+
+            HtmlNodeCollection sizesNodeCollection = document.SelectNodes("//span[contains(@class, 'size_group_code_item')]");
+
+            foreach (var sizeNode in sizesNodeCollection)
+            {
+                details.AddSize(sizeNode.SelectSingleNode("./a").InnerText, "Unknown");
+
+            }
+            return details;
+        }
+
+        private HtmlNode GetWebpage(string url, CancellationToken token)
+        {
+            var client = ClientFactory.GetProxiedFirefoxClient(autoCookies: true);
+            var document = client.GetDoc(url, token).DocumentNode;
+            return client.GetDoc(url, token).DocumentNode;
+        }
+
+        private HtmlNodeCollection GetProductCollection(SearchSettingsBase settings, CancellationToken token)
+        {
+            string url = SearchFormat;
+            var document = GetWebpage(url, token);
+            return document.SelectNodes("//li[@class='item']");
+        }
+
+        private void LoadSingleProduct(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
+        {
+            string name = GetName(item).TrimEnd();
+            string url = GetUrl(item);
+            double price = GetPrice(item);
+            string imageUrl = GetImageUrl(item);
+            var product = new Product(this, name, url, price, imageUrl, url, "USD");
+            if (Utils.SatisfiesCriteria(product, settings))
+            {
+                var keyWordSplit = settings.KeyWords.Split(' ');
+                if (keyWordSplit.All(keyWord => product.Name.ToLower().Contains(keyWord.ToLower())))
+                    listOfProducts.Add(product);
+            }
+        }
+
+        private string GetName(HtmlNode item)
+        {
+            return item.SelectSingleNode("./*[@class='product-name']/a").GetAttributeValue("title", null);
+        }
+
+        private string GetUrl(HtmlNode item)
+        {
+            return item.SelectSingleNode("./h2/a").GetAttributeValue("href", null);
+        }
+
+        private double GetPrice(HtmlNode item)
+        {
+            Match match = Regex.Match(item.InnerHtml, priceRegex);
+            double price = -1;
+            while (match.Success)
+            {
+                price = Convert.ToDouble(match.Groups[1].Value);
+                match = match.NextMatch();
+            }
+            return price;
+        }
+
+        private string GetImageUrl(HtmlNode item)
+        {
+            return item.SelectSingleNode("./div[@class='product-image-wrapper']/a/img").GetAttributeValue("src", null);
+        }
+    }
+}
