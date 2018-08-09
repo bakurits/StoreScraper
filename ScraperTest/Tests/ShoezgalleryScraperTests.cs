85b980a7e2ffc6bd19ce61ff73164f8079cbdeb7
Elie Louis <elie_louis@hotmail.com>
2018-08-09 17:49:19 +0300

    Shoezgallery started.

---
+++ ScraperTest/Tests/ShoezgalleryScraperTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ScraperTest.Tests
+{
+    [TestClass]
+    public class ShoezgalleryScraperTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
