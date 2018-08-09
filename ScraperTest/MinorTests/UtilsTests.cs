42061f4554779b7fa675470886846d63a3064c6a
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-08-09 20:07:18 +0400

    Added price parsed Method in Ultils class, wich returns price an currency from string.

---
+++ ScraperTest/MinorTests/UtilsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreScraper.Helpers;
+
+namespace ScraperTest.MinorTests
+{
+    [TestClass()]
+    public class UtilsTests
+    {
+        [TestMethod()]
+        public void ParsePriceTest()
+        {
+            var prices = new string[] {"500.23 EUR"};
+
+            foreach (var price in prices)
+            {
+                var parsed = Utils.ParsePrice(price);
+                Console.WriteLine(parsed.Value);
+                Console.WriteLine(parsed.Currency);
+            }
+        }
+    }
+}
