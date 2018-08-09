27b356c399b709b6c3c70953ae7f56b119a28551
milan-stanojevic <stanojevic.milan97@gmail.com>
2018-08-09 15:17:15 +0200

    Init UebervartShop

---
+++ Scraper/Bots/UebervartShop/UebervartShopScrapper.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using HtmlAgilityPack;
+using StoreScraper.Factory;
+using StoreScraper.Helpers;
+using StoreScraper.Core;
+using StoreScraper.Models;
+using System.Text.RegularExpressions;
+using System;
+
+namespace StoreScraper.Bots.UebervartShop
+{
+    public class UebervartShopScrapper : ScraperBase
+    {
+        public override string WebsiteName { get; set; } = "UebervartShop";
+        public override string WebsiteBaseUrl { get; set; } = "https://www.uebervart-shop.de";
+        public override bool Active { get; set; }
+
+        private const string noResults = "Sorry, no results found for your searchterm";
+
+        public override void FindItems(out List<Product> listOfProducts, SearchSettingsBase settings, CancellationToken token)
+        {
+            listOfProducts = new List<Product>();
+            HtmlNodeCollection itemCollection = GetProductCollection(settings, token);
+            Console.WriteLine(itemCollection.Count);
+            foreach (var item in itemCollection)
+            {
+                token.ThrowIfCancellationRequested();
+#if DEBUG
+                LoadSingleProduct(listOfProducts, settings, item);
+#else
+                LoadSingleProductTryCatchWraper(listOfProducts, settings, item);
+#endif
+            }
+
+        }
+
+        private void LoadSingleProductTryCatchWraper(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
+        {
+            try
+            {
+                LoadSingleProduct(listOfProducts, settings, item);
+            }
+            catch (Exception e)
+            {
+                Logger.Instance.WriteErrorLog(e.Message);
+            }
+        }
+
+
+        public override ProductDetails GetProductDetails(Product product, CancellationToken token)
+        {
+            var document = GetWebpage(product.Url, token);
+            ProductDetails details = new ProductDetails();
+
+            var sizeCollection = document.SelectNodes("//select[@id='size-select']/option");
+
+            foreach (var size in sizeCollection)
+            {
+                string sz = size.InnerHtml;
+                if (sz.Length > 0)
+                {
+                    details.AddSize(sz, "Unknown");
+                }
+
+            }
+
+            return details;
+        }
+
+        private HtmlNode GetWebpage(string url, CancellationToken token)
+        {
+            var client = ClientFactory.GetProxiedFirefoxClient(autoCookies: true);
+            var document = client.GetDoc(url, token).DocumentNode;
+            return client.GetDoc(url, token).DocumentNode;
+        }
+
+        private HtmlNodeCollection GetProductCollection(SearchSettingsBase settings, CancellationToken token)
+        {
+            //string url = string.Format(SearchFormat, settings.KeyWords);
+            string url = WebsiteBaseUrl + "/en/1551-latest/categories-mens_shoes-womens_shoes-kids_shoes";
+
+            var document = GetWebpage(url, token);
+            if (document.InnerHtml.Contains(noResults)) return null;
+
+            return document.SelectNodes("//a[@class='product']");
+
+        }
+
+        private bool CheckForValidProduct(HtmlNode item, SearchSettingsBase settings)
+        {
+            string title = item.SelectSingleNode("./div[@class='product__name']/h3").InnerText.ToLower();
+            var validKeywords = settings.KeyWords.ToLower().Split(' ');
+            var invalidKeywords = settings.NegKeyWrods.ToLower().Split(' ');
+            foreach (var keyword in validKeywords)
+            {
+                if (!title.Contains(keyword))
+                    return false;
+            }
+
+
+            foreach (var keyword in invalidKeywords)
+            {
+                if (keyword == "")
+                    continue;
+                if (title.Contains(keyword))
+                    return false;
+            }
+
+
+            return true;
+
+        }
+
+        private void LoadSingleProduct(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
+        {
+            //if (!CheckForValidProduct(item, settings)) return;
+            string name = GetName(item).TrimEnd();
+            string url = GetUrl(item);
+            double price = GetPrice(item);
+
+            /*if (!(price >= settings.MinPrice && price <= settings.MaxPrice) && (settings.MaxPrice != 0 && settings.MinPrice != 0))
+            {
+                return;
+            }*/
+
+
+            string imageUrl = GetImageUrl(item);
+            var product = new Product(this, name, url, price, imageUrl, url, "EUR");
+            if (Utils.SatisfiesCriteria(product, settings))
+            {
+                listOfProducts.Add(product);
+            }
+        }
+
+        private bool GetStatus(HtmlNode item)
+        {
+            return true;
+        }
+
+        private string GetName(HtmlNode item)
+        {
+            //Console.WriteLine("GetName");
+            //Console.WriteLine(item.SelectSingleNode("./a").GetAttributeValue("title", ""));
+
+            return item.SelectSingleNode("./div[@class='product__name']/h3").InnerText;
+        }
+
+        private string GetUrl(HtmlNode item)
+        {
+            return WebsiteBaseUrl + item.SelectSingleNode(".").GetAttributeValue("href", null);
+        }
+
+        private double GetPrice(HtmlNode item)
+        {
+            var node = item.SelectSingleNode("./div[@class='product__price']/b");
+            if (node != null)
+            {
+                string priceDiv = item.SelectSingleNode("./div[@class='product__price']/b").InnerHtml.Replace("€", "").Replace("&euro;", "").Replace("$", "").Replace(",", ".");
+
+                return double.Parse(priceDiv);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private string GetImageUrl(HtmlNode item)
+        {
+            return item.SelectSingleNode("./div/img").GetAttributeValue("src", null);
+        }
+    }
+}
+
