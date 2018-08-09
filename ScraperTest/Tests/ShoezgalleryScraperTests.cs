fcba21974fa49de19b6e94f861cec2d4907879a9
Elie Louis <elie_louis@hotmail.com>
2018-08-09 18:38:56 +0300

    Shoesgallery

--- ScraperTest/Tests/ShoezgalleryScraperTests.cs
+++ ScraperTest/Tests/ShoezgalleryScraperTests.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreScraper.Bots.Shoezgallery;
+using StoreScraper.Models;
+
 
 namespace ScraperTest.Tests
 {
@@ -7,8 +11,37 @@ namespace ScraperTest.Tests
     public class ShoezgalleryScraperTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FindItemsTest()
         {
+            ShoezgalleryScraper scraper = new ShoezgalleryScraper();
+            SearchSettingsBase settings = new SearchSettingsBase()
+            {
+                KeyWords = "jordan"
+            };
+
+            scraper.FindItems(out var lst, settings, CancellationToken.None);
         }
+
+        [TestMethod()]
+        public void GetProductDetailsTest()
+        {
+            ShoezgalleryScraper scraper = new ShoezgalleryScraper();
+            Product curProduct = new Product(scraper,
+                "Nike Air Jordan Wmns 1 Retro",
+                "https://www.shoezgallery.com/en/p9727-plato-sneaker-satellite-no-name-plato-sneaker-bandana-black?search_query=sneaker&results=13",
+                1,
+                "whatever",
+                "whatever",
+                "EUR");
+
+            ProductDetails details = scraper.GetProductDetails(curProduct, CancellationToken.None);
+            foreach (var sz in details.SizesList)
+            {
+                Debug.WriteLine(sz);
+            }
+        }
+
     }
+
+    //https://www.shoezgallery.com/en/p9727-plato-sneaker-satellite-no-name-plato-sneaker-bandana-black?search_query=sneaker&results=13
 }
