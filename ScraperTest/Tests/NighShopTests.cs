88f270e4bd950a9797e10c2e0c0abf15712c90e2
Elie Louis <elie_louis@hotmail.com>
2018-08-09 14:27:48 +0300

    Nighshop done.

--- ScraperTest/Tests/NighShopTests.cs
+++ ScraperTest/Tests/NighShopTests.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreScraper.Bots.Nighshop;
+using StoreScraper.Models;
 
 namespace ScraperTest.Tests
 {
@@ -7,8 +10,33 @@ namespace ScraperTest.Tests
     public class NighShopTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FindItemsTest()
         {
+            NighshopScraper scraper = new NighshopScraper();
+            SearchSettingsBase settings = new SearchSettingsBase()
+            {
+                KeyWords = "jordan"
+            };
+
+            scraper.FindItems(out var lst, settings, CancellationToken.None);
+        }
+        [TestMethod()]
+        public void GetProductDetailsTest()
+        {
+            NighshopScraper scraper = new NighshopScraper();
+            Product curProduct = new Product(scraper,
+                "Nike Air Jordan Wmns 1 Retro",
+                "https://www.nighshop.com/adidas-by-raf-simons-replicant-ozweego-belgium-schwarz-rot-gelb-f34234",
+                1,
+                "whatever",
+                "whatever",
+                "EUR");
+
+            ProductDetails details = scraper.GetProductDetails(curProduct, CancellationToken.None);
+            foreach (var sz in details.SizesList)
+            {
+                Debug.WriteLine(sz);
+            }
         }
     }
 }
