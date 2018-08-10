8d9c681bedbc00930fa1e9c036327d1e02d2f96b
milan-stanojevic <stanojevic.milan97@gmail.com>
2018-08-10 04:10:10 +0200

    Added Snkrs Test

---
+++ ScraperTest/ScraperTests/Mstanojevic/SnkrsTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScraperTest.Helpers;
+using StoreScraper.Bots.Mstanojevic.Snkrs;
+using StoreScraper.Models;
+
+namespace ScraperTest.ScraperTests.Mstanojevic
+{
+    [TestClass]
+    public class SnkrsTest
+    {
+        [TestMethod()]
+        public void FindItemsTest()
+        {
+            SnkrsScrapper scraper = new SnkrsScrapper();
+            SearchSettingsBase settings = new SearchSettingsBase();
+            settings.KeyWords = "nike";
+
+
+            scraper.FindItems(out var lst, settings, CancellationToken.None);
+            Helpers.Helper.PrintFindItemsResults(lst);
+
+        }
+
+        [TestMethod()]
+        public void GetProductDetailsTest()
+        {
+            Product curProduct = new Product(new SnkrsScrapper(), "Unknown",
+                "https://www.snkrs.com/en/nike/m2k-tekno-blackoff-white-8222.html",
+                420,
+                "https://media.snkrs.com/43202-thickbox/m2k-tekno-blackoff-white.jpg",
+                "id");
+
+
+            SnkrsScrapper scraper = new SnkrsScrapper();
+
+            ProductDetails details = scraper.GetProductDetails(curProduct, CancellationToken.None);
+
+            Helper.PrintGetDetailsResult(details.SizesList);
+            Debug.WriteLine(curProduct.Name);
+
+        }
+    }
+}
