97ce2c702fc2bd28d7b683d5a43f0e2192bece9d
Elie Louis <elie_louis@hotmail.com>
2018-08-11 10:38:28 +0300

    Chmielna GetProductDetails updated & minor changes in the code.

--- Scraper/Bots/Higuhigu/Chmielna/ChmielnaScraper.cs
+++ Scraper/Bots/Higuhigu/Chmielna/ChmielnaScraper.cs
@@ -7,6 +7,8 @@ using StoreScraper.Core;
 using StoreScraper.Factory;
 using StoreScraper.Helpers;
 using StoreScraper.Models;
+using System.Net;
+using System.Linq;
 
 namespace StoreScraper.Bots.Higuhigu.Chmielna
 {
@@ -35,6 +37,33 @@ namespace StoreScraper.Bots.Higuhigu.Chmielna
 
         }
 
+        private HtmlDocument GetWebpage(string url, CancellationToken token)
+        {
+            var client = ClientFactory.GetProxiedFirefoxClient(autoCookies: true);
+            var document = client.GetDoc(url, token);
+            return document;
+        }
+
+        private HtmlNodeCollection GetProductCollection(SearchSettingsBase settings, CancellationToken token)
+        {
+            string url = string.Format(SearchFormat, settings.KeyWords);
+            var document = GetWebpage(url, token);
+            if (document == null)
+            {
+                Logger.Instance.WriteErrorLog($"Can't Connect to einhalb website");
+                throw new WebException("Can't connect to website");
+            }
+            var node = document.DocumentNode;
+            var items = node.SelectNodes("//div[contains(@class, 'products__item')]");
+            if (items == null)
+            {
+                Logger.Instance.WriteErrorLog("Uncexpected Html!!");
+                Logger.Instance.SaveHtmlSnapshop(document);
+                throw new WebException("Undexpected Html");
+            }
+            return items;
+        }
+
         private void LoadSingleProductTryCatchWraper(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
         {
             try
@@ -47,48 +76,13 @@ namespace StoreScraper.Bots.Higuhigu.Chmielna
             }
         }
 
-        public override ProductDetails GetProductDetails(string productUrl, CancellationToken token)
-        {
-
-            var document = GetWebpage(productUrl, token);
-            ProductDetails details = new ProductDetails();
-
-            var sizeCollection = document.SelectNodes("//div[@class='selector']/ul/li");
-
-            foreach (var size in sizeCollection)
-            {
-                string sz = size.GetAttributeValue("data-sizeeu", null);
-                if (sz == null) continue;
-                if (sz.Length > 0)
-                {
-                    details.AddSize(sz, "Unknown");
-                }
-
-            }
-            return details;
-        }
-
-        private HtmlNode GetWebpage(string url, CancellationToken token)
-        {
-            var client = ClientFactory.GetProxiedFirefoxClient(autoCookies: true);
-            var document = client.GetDoc(url, token).DocumentNode;
-            return document;
-        }
-
-        private HtmlNodeCollection GetProductCollection(SearchSettingsBase settings, CancellationToken token)
-        {
-            string url = string.Format(SearchFormat, settings.KeyWords);
-            var document = GetWebpage(url, token);
-            return document.SelectNodes("//div[contains(@class, 'products__item')]");
-        }
-
         private void LoadSingleProduct(List<Product> listOfProducts, SearchSettingsBase settings, HtmlNode item)
         {
             string name = GetName(item).TrimEnd();
             string url = GetUrl(item);
-            double price = GetPrice(item);
+            var price = GetPrice(item);
             string imageUrl = GetImageUrl(item);
-            var product = new Product(this, name, url, price, imageUrl, url, "EUR");
+            var product = new Product(this, name, url, price.Value, imageUrl, url, price.Currency);
             if (Utils.SatisfiesCriteria(product, settings))
             {
                 listOfProducts.Add(product);
@@ -105,22 +99,53 @@ namespace StoreScraper.Bots.Higuhigu.Chmielna
             return item.SelectSingleNode("./a").GetAttributeValue("href", null);
         }
 
-        private double GetPrice(HtmlNode item)
+        private Price GetPrice(HtmlNode item)
         {
-            string priceP = item.SelectSingleNode(".//p[@class='products__item-price']").InnerHtml;
-            Match match = Regex.Match(priceP, "(\\d+(\\.\\d+)?)");
-            double price = -1;
-            while (match.Success)
-            {
-                price = Convert.ToDouble(match.Groups[1].Value);
-                match = match.NextMatch();
-            }
-            return price;
+            string priceP = item.SelectSingleNode(".//p[@class='products__item-price']/span[last()]").InnerText;
+            return Utils.ParsePrice(priceP);
         }
 
         private string GetImageUrl(HtmlNode item)
         {
             return item.SelectSingleNode(".//img").GetAttributeValue("src", null);
+        }
+
+        public override ProductDetails GetProductDetails(string productUrl, CancellationToken token)
+        {
+
+            var document = GetWebpage(productUrl, token);
+            if (document == null)
+            {
+                Logger.Instance.WriteErrorLog($"Can't Connect to einhalb website");
+                throw new WebException("Can't connect to website");
+            }
+
+            var root = document.DocumentNode;
+            var sizeNodes = root.SelectNodes("//div[@class='selector']/ul/li");
+            var sizes = sizeNodes.Select(node => node.GetAttributeValue("data-sizeeu", null)).ToList();
+
+            var name = root.SelectSingleNode("//div[@class='product__name']/h1").InnerText.Trim();
+            var priceNode = root.SelectSingleNode("//span[@class='product__price_shop']");
+            var price = Utils.ParsePrice(priceNode.InnerText);
+            var image = root.SelectSingleNode("//div[@class='item zoom']/img").GetAttributeValue("src", null);
+
+            ProductDetails result = new ProductDetails()
+            {
+                Price = price.Value,
+                Name = name,
+                Currency = price.Currency,
+                ImageUrl = image,
+                Url = productUrl,
+                Id = productUrl,
+                ScrapedBy = this
+            };
+
+            foreach (var size in sizes)
+            {
+                result.AddSize(size, "Unknown");
+            }
+
+            return result;
         }
     }
 }
