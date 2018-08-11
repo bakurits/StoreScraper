25471cd55f1e6dd25415b0dbd4c98d15719b80d1
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-08-11 18:26:12 +0400

    Minor changes

--- ScraperTest/Minortests/DiscordWebhookTests.cs
+++ ScraperTest/Minortests/DiscordWebhookTests.cs
@@ -9,14 +9,14 @@ namespace ScraperTest.MinorTests
     [TestClass]
     public class DiscordWebhookTests
     {
-        [TestMethod]
+        //[TestMethod]
         public void SendTest()
         {
             Product product = new Product(new ShelflifeScraper(), "Vans x Van Gogh UA SK8-Hi - Almond Blossom/True White",
                 "https://www.shelflife.co.za/products/Vans-x-Van-Gogh-UA-SK8-Hi-Almond-Blossom-True-White",
                 1399.00,
                 "https://www.shelflife.co.za/pics/product/large/vn0a38geubl-side.jpg",
-                "id", "R");
+                "id", "£");
             var task = DiscordWebhook.Send(
                 "https://discordapp.com/api/webhooks/468240680414609429/kKJB9L4I8AfQWWDcqf0vpAj9OYDqxLAJ9gHl1b2B5xg8c5X2Ic4FpcSHAE8_0vKqZBoP",
                 product);
--- ScraperTest/Minortests/SlackWebHookTests.cs
+++ ScraperTest/Minortests/SlackWebHookTests.cs
@@ -10,14 +10,14 @@ namespace ScraperTest.MinorTests
     [TestClass()]
     public class SlackWebHookTests
     {
-        [TestMethod()]
+        //[TestMethod()]
         public void PostMessageTest()
         {
             Product product = new Product(new MrporterScraper(), "JOHN ELLIOTT Camp-Collar Printed Tencel-Twill Shirt",
                 "https://www.mrporter.com/mens/okeeffe/bristol-leather-trimmed-suede-derby-shoes/1026175",
                 120.83,
                 "https://cache.mrporter.com/images/products/1012326/1012326_mrp_in_l.jpg",
-                "id","$");
+                "id","£");
             var task = SlackWebHook.PostMessage(product,
                 "https://hooks.slack.com/services/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");
 
