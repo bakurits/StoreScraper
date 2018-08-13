829f9516598c109c53fcdbe4c765233202a9e3f3
bakurits <bakuricucxashvili@gmail.com>
2018-08-13 15:38:10 +0400

    Some changes hoocks now getting ProductDetails instead of Product
    @gchkh16 review needed

--- ScraperTest/Minortests/SlackWebHookTests.cs
+++ ScraperTest/Minortests/SlackWebHookTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StoreScraper.Bots.Bakurits.Mrporter;
+using StoreScraper.Bots.Bakurits.Shelflife;
 using StoreScraper.Helpers;
 using StoreScraper.Models;
 
@@ -10,15 +12,26 @@ namespace ScraperTest.MinorTests
     [TestClass()]
     public class SlackWebHookTests
     {
-        //[TestMethod()]
+        [TestMethod()]
         public void PostMessageTest()
         {
-            Product product = new Product(new MrporterScraper(), "JOHN ELLIOTT Camp-Collar Printed Tencel-Twill Shirt",
-                "https://www.mrporter.com/mens/okeeffe/bristol-leather-trimmed-suede-derby-shoes/1026175",
-                120.83,
-                "https://cache.mrporter.com/images/products/1012326/1012326_mrp_in_l.jpg",
-                "id","£");
-            var task = new SlackPoster().PostMessage("https://hooks.slack.com/services/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", product, CancellationToken.None);
+            ProductDetails productDetails = new ProductDetails
+            {
+                ScrapedBy = new ShelflifeScraper(),
+                Name = "Vans x Van Gogh UA SK8-Hi - Almond Blossom/True White",
+                Url = "https://www.shelflife.co.za/products/Vans-x-Van-Gogh-UA-SK8-Hi-Almond-Blossom-True-White",
+                Price = 1399.00,
+                ImageUrl = "https://www.shelflife.co.za/pics/product/large/vn0a38geubl-side.jpg",
+                Id = "https://www.shelflife.co.za/products/Vans-x-Van-Gogh-UA-SK8-Hi-Almond-Blossom-True-White",
+                Currency = "£",
+                SizesList = new List<StringPair>
+                {
+                    new StringPair() {Key = "L", Value = "1"},
+                    new StringPair() {Key = "XL", Value = "1"},
+                    new StringPair() {Key = "XXL", Value = "1"}
+                }
+            };
+            var task = new SlackPoster().PostMessage("https://hooks.slack.com/services/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", productDetails, CancellationToken.None);
 
 
             try
@@ -29,9 +42,6 @@ namespace ScraperTest.MinorTests
             {
                 Debug.WriteLine(task.Result.Content.ReadAsStringAsync().Result);
             }
-
-            Thread.Sleep(5000);
-
         }
     }
 }
