eeab8230d190e2ed4ffe1f4158c486c8f13936bb
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-08-14 10:49:09 +0400

    Defined basic structure of checkout bot

---
+++ CheckoutBot/Bots/ChampsSports/ChampsSportsBot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CheckoutBot.Interfaces;
+using CheckoutBot.Models;
+using StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker;
+
+namespace CheckoutBot.Bots.ChampsSports
+{
+    class ChampsSportsBot : FootSimpleBase.ChampsSportsScraper, IGuestCheckouter, IAccountCheckouter
+    {
+        public void GuestCheckOut(GuestCheckoutSettings settings, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AccountCheckout(AccountCheckoutSettings settings, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutBot/Models/GuestCheckoutSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreScraper.Models;
+
+namespace CheckoutBot.Models
+{
+    public class GuestCheckoutSettings
+    {
+        public ShippinInfo Shipping;
+        public CartInfo CartInfo;
+        public Product ProductToBuy;
+        public ProductBuyOptions BuyOptions;
+    }
+
+}
