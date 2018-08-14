fcfa5ea0adf60c20f851e63d7487bb004c5eee91
milan-stanojevic <stanojevic.milan97@gmail.com>
2018-08-14 13:47:01 +0200

    Added color filter for champssports.com, working on eastbay  i need to fetch color ids (different color ids on champssports and eastbay)

--- ScraperCore/Bots/Sticky_bit/ChampsSports_FootLocker/FootStoreBase.cs
+++ ScraperCore/Bots/Sticky_bit/ChampsSports_FootLocker/FootStoreBase.cs
@@ -23,6 +23,15 @@ namespace StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker
         public string crumbs { get; set; }
     }
 
+    public class Color
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string cmRef { get; set; }
+        public string crumbs { get; set; }
+
+    }
+
     [DisabledScraper]
     public class FootSimpleBase : ScraperBase
     {
@@ -33,11 +42,12 @@ namespace StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker
         private string UrlPrefix;
         private const string PageSizeSuffix = @"?Ns=P_NewArrivalDateEpoch%7C1&cm_SORT=New%20Arrivals";
         private const string Keywords = @"/keyword-{0}";
-        private const string GenderPrefix = @"/{0}/_-_/N-{1}";
+        private const string GenderColorPrefix = @"{0}/_-_/N-{1}";
         private const string GenderPostfix = @"&crumbs={0}&cm_REF={1}";
         private const string UlXpath = @"//*[@id=""endeca_search_results""]/ul";
 
         Dictionary<int, Gender> genders;
+        Dictionary<int, Color> colors;
 
 
         public FootSimpleBase(string websiteName, string websiteBaseUrl)
@@ -54,6 +64,24 @@ namespace StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker
                     { 2, new Gender {name="Womens", id="25",crumbs="77", cmRef="Women%27s"}},
                     { 3, new Gender {name="Boys", id="22",crumbs="2689", cmRef="Boys%27"}},
                     { 4, new Gender {name="Girls", id="25",crumbs="2690", cmRef="Girls%27"}},
+                };
+
+                colors = new Dictionary<int, Color>()
+                {
+                    { 1, new Color {name="Pink", id="dx",crumbs="501", cmRef="Pink"}},
+                    { 2, new Color {name="Red", id="di",crumbs="486", cmRef="Red"}},
+                    { 3, new Color {name="Orange", id="do",crumbs="492", cmRef="Orange"}},
+                    { 4, new Color {name="Yellow", id="dy",crumbs="502", cmRef="Yellow"}},
+                    { 5, new Color {name="Green", id="dg",crumbs="484", cmRef="Green"}},
+                    { 6, new Color {name="Blue", id="df",crumbs="483", cmRef="Blue"}},
+                    { 7, new Color {name="Pruple", id="dj",crumbs="487", cmRef="Purple"}},
+                    { 8, new Color {name="Black", id="de",crumbs="482", cmRef="Black"}},
+                    { 9, new Color {name="Grey", id="ds",crumbs="496", cmRef="Grey"}},
+                    { 10, new Color {name="White", id="dl",crumbs="489", cmRef="White"}},
+                    { 11, new Color {name="Tan", id="du",crumbs="498", cmRef="Tan"}},
+                    { 12, new Color {name="Brown", id="dv",crumbs="499", cmRef="Brown"}},
+                    { 13, new Color {name="Multicolor", id="e3",crumbs="507", cmRef="Multicolor"}},
+
                 };
             }
             else if (websiteName == "EastBay")
@@ -82,11 +110,60 @@ namespace StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker
 
             string searchUrl = UrlPrefix + string.Format(Keywords, settings.KeyWords) + PageSizeSuffix;
 
-            int gender = 1; // 0 - all, 1 - mens, 2 - womens, 3 - boys, 4 - girls 
-            if (gender != 0)
+            //int genderId = 2; // 0 - all, 1 - mens, 2 - womens, 3 - boys, 4 - girls 
+            //int colorId = 9;
+            int genderId = 0;
+            int colorId = 0;
+
+            if (genderId != 0 || colorId != 0)
             {
-                searchUrl = WebsiteBaseUrl + string.Format(GenderPrefix, genders[gender].name, genders[gender].id) + string.Format(Keywords, settings.KeyWords) + PageSizeSuffix + string.Format(GenderPostfix, genders[gender].crumbs, genders[gender].cmRef);
+                Color color = null;
+                Gender gender = null;
+
+                string genderString = "";
+                string crumbs = "";
+                string cmREF;
+                if (genderId != 0)
+                {
+                    gender = genders[genderId];
+                    genderString = "/" + gender.name;
+                }
+
+                if (colorId != 0)
+                {
+                    color = colors[colorId];
+                }
+
+
+                string colorGenderId = "";
+
+                if (color != null && gender != null)
+                {
+                    colorGenderId = gender.id + "Z" + color.id;
+                    crumbs = gender.crumbs + " " + color.crumbs;
+                    cmREF = color.cmRef;
+                }
+                else if (color != null)
+                {
+                    colorGenderId = color.id;
+                    crumbs = color.crumbs;
+                    cmREF = color.cmRef;
+
+                }
+                else
+                {
+                    colorGenderId = gender.id;
+                    crumbs = gender.crumbs;
+                    cmREF = gender.cmRef;
+
+                }
+
+
+                searchUrl = WebsiteBaseUrl + string.Format(GenderColorPrefix, genderString, colorGenderId) + string.Format(Keywords, settings.KeyWords) + PageSizeSuffix + string.Format(GenderPostfix, crumbs, cmREF);
             }
+
+            
+
 
             HtmlNode container = null;
 
