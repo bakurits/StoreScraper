74a8f60cb74fac413c7a139633b9314033268554
milan-stanojevic <stanojevic.milan97@gmail.com>
2018-08-16 00:04:35 +0200

    Styling listview

--- CheckoutBot/MainWindow.xaml.cs
+++ CheckoutBot/MainWindow.xaml.cs
@@ -22,6 +22,40 @@ namespace CheckoutBot
         public MainWindow()
         {
             InitializeComponent();
+
+            List<TaskItem> items = new List<TaskItem>();
+            items.Add(new TaskItem() { Keywords = "nike air", Size = 12, Retries = "1", Status="Checking out" });
+            items.Add(new TaskItem() { Keywords = "adidas", Size = 7, Retries = "3", Status = "Error" });
+            items.Add(new TaskItem() { Keywords = "puma", Size = 8, Retries = "0", Status = "Done" });
+            tasksList.ItemsSource = items;
+
         }
+
+
+        private void handleAccountType(object sender, RoutedEventArgs e)
+        {
+            if (userRadioBtn.IsChecked == true)
+            {
+                accComboBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                accComboBox.Visibility = Visibility.Hidden;
+            }
+        }
+
+      
     }
+
+    public class TaskItem
+    {
+        public string Keywords { get; set; }
+
+        public int Size { get; set; }
+
+        public string Retries { get; set; }
+        public string Status { get; set; }
+
+    }
+
 }
