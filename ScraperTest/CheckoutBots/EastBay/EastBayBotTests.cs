75a2613b98bc18ba637924d1130cf42787ab5034
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-08-17 19:52:05 +0400

    Bug Fix.

---
+++ ScraperTest/CheckoutBots/EastBay/EastBayBotTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutBot.CheckoutBots.EastBay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutBotTests
+{
+    [TestClass()]
+    public class EastBayBotTests
+    {
+        [TestMethod()]
+        public void GuestCheckOutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void AccountCheckoutTest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod()]
+        public void ScrapeReleasePageTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
