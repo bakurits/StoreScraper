424839890d6a308eb553f8bee83d194f7627822e
Luka Tchumburidze <lukatchumburidze@gmail.com>
2018-08-18 23:40:07 +0400

    Disintegrated FootScraperTest class into 4 classes

---
+++ ScraperTest/ScraperTests/Sticky_bit/FootSites/ChampsSportsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScraperTest.Helpers;
+using StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker;
+using StoreScraper.Models;
+
+namespace ScraperTest.ScraperTests.Sticky_bit.FootSites
+{
+    [TestClass]
+    class ChampsSportsTest
+    {
+        [TestMethod]
+        public void FindItems()
+        {
+            FootSimpleBase.ChampsSportsScraper scraper = new FootSimpleBase.ChampsSportsScraper();
+            scraper.FindItems(out var lst, Helper.SearchSettings, CancellationToken.None);
+            Helper.PrintFindItemsResults(lst);
+        }
+
+        public void GetProductDetails()
+        {
+            FootSimpleBase.ChampsSportsScraper scraper = new FootSimpleBase.ChampsSportsScraper();
+            ProductDetails productDetails = scraper.GetProductDetails("https://www.champssports.com/product/model:258371/sku:B76079/adidas-originals-nmd-r1-mens/grey/white/#sku-B76079", CancellationToken.None);
+            Console.WriteLine(productDetails);
+        }
+    }
+}
---
+++ ScraperTest/ScraperTests/Sticky_bit/FootSites/EastBayTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScraperTest.Helpers;
+using StoreScraper.Bots.Sticky_bit.ChampsSports_FootLocker;
+using StoreScraper.Models;
+
+namespace ScraperTest.ScraperTests.Sticky_bit.FootSites
+{
+    [TestClass]
+    class EastBayTest
+    {
+
+        [TestMethod]
+        public void FindItems()
+        {
+            FootSimpleBase.EastBayScraper scraper = new FootSimpleBase.EastBayScraper();
+            scraper.FindItems(out var lst, Helper.SearchSettings, CancellationToken.None);
+            Helper.PrintFindItemsResults(lst);
+        }
+
+        [TestMethod]
+        public void GetProductDetails()
+        {
+            FootSimpleBase.EastBayScraper scraper = new FootSimpleBase.EastBayScraper();
+            ProductDetails productDetails = scraper.GetProductDetails("https://www.eastbay.com/product/model:286098/sku:A111794/easton-ghost-x-bbcor-baseball-bat-mens/white/gold/", CancellationToken.None);
+            Console.Write(productDetails); 
+        }
+    }
+}
