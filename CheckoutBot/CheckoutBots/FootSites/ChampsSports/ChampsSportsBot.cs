4c01a2eeafb7179597bf4d041b4e61c8b0f93ad5
Davit Bezhanishvili <dbezh16@freeuni.edu.ge>
2018-08-22 22:59:06 +0000

    changes to champssportsBot login method

--- CheckoutBot/CheckoutBots/FootSites/ChampsSports/ChampsSportsBot.cs
+++ CheckoutBot/CheckoutBots/FootSites/ChampsSports/ChampsSportsBot.cs
@@ -4,6 +4,10 @@ using System.Threading;
 using CheckoutBot.Interfaces;
 using CheckoutBot.Models;
 using CheckoutBot.Models.Checkout;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using StoreScraper.Http.Factory;
+using static OpenQA.Selenium.Support.UI.ExpectedConditions;
 
 namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
 {
@@ -16,9 +20,39 @@ namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
         {
             throw new NotImplementedException();
         }
+        private IWebElement GetVisibleElementByXPath(WebDriverWait wait, string xPath, CancellationToken token)
+        {
+            var element = wait.Until(ElementIsVisible(By.XPath(xPath)));
+            token.ThrowIfCancellationRequested();
+            return element;
+        }
+        private IWebElement GetClickableElementByXPath(WebDriverWait wait, string xPath, CancellationToken token)
+        {
+            var element = wait.Until(ElementToBeClickable(By.XPath(xPath)));
+            token.ThrowIfCancellationRequested();
+            return element;
+        }
+
 
         public override HttpClient Login(string username, string password, CancellationToken token)
         {
+
+            var driver = ClientFactory.CreateProxiedFirefoxDriver(true);
+            driver.Navigate().GoToUrl(this.WebsiteBaseUrl);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+
+            var loginPopupButton = GetClickableElementByXPath(wait, "//div[@id='header_login']/a", token);
+            loginPopupButton.Click();
+
+            var emailTextBox = GetVisibleElementByXPath(wait, "//input[@id='login_email']", token);
+            emailTextBox.SendKeys(username);
+
+
+            var passwordTextBox = GetVisibleElementByXPath(wait, "//input[@id='login_password']", token);
+            passwordTextBox.SendKeys(password);
+
+            var signinButton = GetClickableElementByXPath(wait, "//div[@class='submit']/button", token);
+            signinButton.Click();
             throw new NotImplementedException();
         }
 
