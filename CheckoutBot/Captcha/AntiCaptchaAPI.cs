62818d0a27cc383044b7f1340621b28f7424899b
Elie Louis <elie_louis@hotmail.com>
2018-08-22 13:01:48 +0300

    AntiCaptcha added.

---
+++ CheckoutBot/Captcha/AntiCaptchaAPI.cs
@@ -0,0 +1,40 @@
+﻿using CheckoutBot.Captcha;
+using Nuget_AntiCaptcha;
+using Nuget_AntiCaptcha.Responses;
+using Nuget_AntiCaptcha.Captchas;
+using System.Threading.Tasks;
+
+namespace CheckoutBot.Anti_Captcha
+{
+    public class AntiCaptchaAPI : CaptchaAPIBase
+    {
+        public async override Task<string> GetCaptchaResponse(string siteKey, string url)
+        {
+            AntiCaptcha Client = new AntiCaptcha(_apiKey);
+            NoCaptchaTaskProxyless task = new NoCaptchaTaskProxyless
+            {
+                websiteKey = siteKey,
+                websiteURL = url
+            };
+            TaskResponse response = await Client.SubmitTask(task);
+
+            while (true)
+            {
+                NoCaptchaSolution solution = await Client.GetNoCaptchaSolution(response.taskId);
+                if (solution.errorId != 0)
+                {
+                    return "";
+                }
+                if (solution.status == "ready")
+                {
+                    return solution.solution.gRecaptchaResponse;
+                }
+            }
+        }
+
+        public AntiCaptchaAPI(string apiKey) : base(apiKey)
+        {
+
+        }
+    }
+}
---
+++ ScraperTest/CaptchaTests/AntiCaptchaTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutBot.Anti_Captcha;
+
+namespace ScraperTest.CaptchaTests
+{
+    [TestClass]
+    public class AntiCaptchaTest
+    {
+        [TestMethod]
+        public async System.Threading.Tasks.Task TestMethod1Async()
+        {
+            var captchaHandler = new AntiCaptchaAPI("ca253acc9bd077d1afbb00d422e94a65");
+            string siteKey = "6Ldp2bsSAAAAAAJ5uyx_lx34lJeEpTLVkP5k04qc";
+            string url = "http://boards.4chan.org/";
+            string result = captchaHandler.GetCaptchaResponse(siteKey, url).Result;
+            var test = result;
+        }
+    }
+}
