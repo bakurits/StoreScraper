fa1121f5f409a1e58345be87e3ae944c5c52c391
Davit Bezhanishvili <dbezh16@freeuni.edu.ge>
2018-08-26 19:48:15 +0000

    minor changes

---
+++ ScraperTest/CheckoutBots/FootSites/checkReleaseTimeTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CheckoutBot.CheckoutBots.FootSites;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreScraper.Models;
+
+namespace ScraperTest.CheckoutBots.FootSites
+{
+    [TestClass]
+    public class checkReleaseTimeTest
+    {
+        [TestMethod]
+        public void check()
+        {
+            CheckRelaeseDates checker = new CheckRelaeseDates();
+            checker.checkRelaese();
+        }
+
+        [TestMethod]
+        public void testCheckPost()
+        {
+            CheckRelaeseDates checker = new CheckRelaeseDates();
+            Product product = new Product(null, "sd", "https://www.eastbay.com/product/model:303959/sku:00500617/", 0, "", "", "", null);
+            checker.checkPost(product, CancellationToken.None);
+        }
+    }
+}
