c356555c380f060f6cbcaeaaea335844f39f4d10
Giorgi <g.chxikvadze.14.12@gmail.com>
2018-09-18 16:48:50 +0400

    Proxy checker header added

---
+++ CheckoutBot/Interfaces/IProxyChecker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace CheckoutBot.Interfaces
+{
+    public interface IProxyChecker
+    {
+        /// <summary>
+        /// Chooses best proxies from proxy pool. Proxies which is detected by website must be filtered
+        /// <remarks>
+        /// returned proxies should be sorter in response time order.
+        /// So proxies on which website responds faster should have smaller index in list
+        /// </remarks>
+        /// </summary>
+        /// <param name="proxyPool"></param>
+        /// <param name="maxCount">max number of proxies that should be returned</param>
+        /// <returns></returns>
+        List<WebProxy> ChooseBestProxies(List<WebProxy> proxyPool, int maxCount);
+    }
+}
