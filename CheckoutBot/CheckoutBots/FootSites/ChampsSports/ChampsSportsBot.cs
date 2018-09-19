fbb2735c917213636a1a6aba71899b968c459fde
bakurits <bakuricucxashvili@gmail.com>
2018-09-19 19:55:47 +0400

    add some methods

--- CheckoutBot/CheckoutBots/FootSites/ChampsSports/ChampsSportsBot.cs
+++ CheckoutBot/CheckoutBots/FootSites/ChampsSports/ChampsSportsBot.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using CheckoutBot.Core;
+using CheckoutBot.Models;
 using CheckoutBot.Models.Checkout;
+using EO.WebBrowser;
+using Newtonsoft.Json.Linq;
+using StoreScraper.Core;
+using StoreScraper.Helpers;
 using StoreScraper.Http.Factory;
 
 namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
@@ -12,6 +18,7 @@ namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
     public class ChampsSportsBot : FootSitesBotBase
     {
         private const string ApiUrl = "http://pciis02.eastbay.com/api/v2/productlaunch/ReleaseCalendar/20";
+        private const string CartUrl = "https://www.eastbay.com/shoppingcart";
         public int DelayInSecond { private get; set; } = 5;
 
         public ChampsSportsBot() : base("ChampsSports", "https://www.champssports.com/", ApiUrl)
@@ -21,9 +28,111 @@ namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
 
         public override void AccountCheckout(AccountCheckoutSettings settings, CancellationToken token)
         {
-            Browser = new EOBrowserDriver();
             Browser.NewTab("MainTab");
-            Login(settings.UserLogin, settings.UserPassword, token);
+
+            if (!Login("gbagh16@freeuni.edu.ge", "giorgi121", token))
+            {
+                Logger.Instance.WriteErrorLog("Wrong password");
+                return;
+            }
+
+            FootsitesProduct arbitraryProduct = GetArbitraryItem(token);
+            var cartTab = Browser.NewTab("Cart");
+            AddArbitraryItemToCart(arbitraryProduct, token);
+            Task.Delay(DelayInSecond * 1000, token).Wait(token);
+            AddToCart(Browser.ActiveTab, settings, token);
+            Task.Delay(5 * 1000).Wait(token);
+/*
+            RemoveArbitraryItem(cartTab, arbitraryProduct, token);
+            Task.Delay(DelayInSecond * 1000, token).Wait(token);
+            Browser.SwitchToTab(0).Reload().WaitOne();
+            Task.Delay(10 * 1000, token).Wait(token);
+*/
+
+        }
+        
+        /// <summary>
+        /// This method adds Arbitrary item to cart
+        /// </summary>
+        /// <param name="product"> Item to add </param>
+        /// <param name="token"></param>
+        private void AddArbitraryItemToCart(FootsitesProduct product, CancellationToken token)
+        {
+            AddToCart(Browser.ActiveTab, new AccountCheckoutSettings()
+            {
+                ProductToBuy = product,
+                BuyOptions = new ProductBuyOptions()
+                {
+                    Quantity = 1,
+                    Size = product.Sizes[0]
+                }
+            }, token);
+            Browser.ActiveTab.LoadUrlAndWait(CartUrl);
+            Browser.ActiveTab.EvalScript("document.getElementById(\"cta_button\").click();");
+            Task.Delay(10000, token).Wait(token);
+        }
+
+        
+        protected static string AjaxGetRequest(string url)
+        {
+            return $@"
+                        var xhr = new XMLHttpRequest();
+                        var date = Date.now();
+                        xhr.open('GET', {url});
+                        xhr.onload = function() {{
+                            if (xhr.status === 200) {{
+                                console.log(xhr.responseText);
+                            }} else {{
+                                alert('Request failed.  Returned status of ' + xhr.status);
+                            }}
+                        }};
+                        xhr.send();";
+        }
+
+        
+        /// <summary>
+        /// This method adds product to cart
+        /// </summary>
+        /// <param name="driver"> driver from which scripts are called </param>
+        /// <param name="settings"> Checkout settings </param>
+        /// <param name="token"></param>
+        private void AddToCart(WebView driver, AccountCheckoutSettings settings, CancellationToken token)
+        {
+            //Console.WriteLine(settings);
+            driver.LoadUrlAndWait(settings.ProductToBuy.Url);
+            
+            driver.EvalScript(AjaxGetRequest($@"'https://www.champssports.com/pdp/gateway?requestKey=' + requestKey + '&action=add&qty={settings.BuyOptions.Quantity}&sku={settings.ProductToBuy.Sku}&size={settings.BuyOptions.Size}&fulfillmentType=SHIP_TO_HOME&storeNumber=00000&storeCostOfGoods=0.00&_=1537363416933'"));
+            Task.Delay(2000, token).Wait(token);
+        }
+
+        
+        /// <summary>
+        /// This method gets random item from releases page
+        /// </summary>
+        private FootsitesProduct GetArbitraryItem(CancellationToken token)
+        {
+            FootsitesProduct product = ScrapeReleasePage(token)[0];
+            GetProductSizes(product, token);
+            return product;
+        }
+        
+        public void GetProductSizes(FootsitesProduct product, CancellationToken token)
+        {
+            List<string> infos = new List<string>();
+            var client = ClientFactory.CreateHttpClient(autoCookies: true).AddHeaders(ClientFactory.FireFoxHeaders);
+            var document = client.GetDoc(product.Url, token).DocumentNode;
+            int ind = document.InnerHtml.IndexOf("var styles = ", StringComparison.Ordinal);
+            var sizeData = Utils.GetFirstJson(document.InnerHtml.Substring(ind));
+            var sizesForCurProd = (JArray)sizeData[product.Sku][7];
+
+            foreach (var item in sizesForCurProd)
+            {
+                var t = (JArray) item;
+                var s = (string)t[0];
+                infos.Add(s.Trim());
+            }
+
+            product.Sizes = infos;
         }
 
         public override void GuestCheckOut(GuestCheckoutSettings settings, CancellationToken token)
@@ -38,12 +147,14 @@ namespace CheckoutBot.CheckoutBots.FootSites.ChampsSports
             webView.EvalScript(GetScriptByXpath("//div[@id='header_login']") + ".click();");
             Task.Delay(10 * 1000, token).Wait(token);
             webView.EvalScript($"{getElementById("login_email")}.value='{username}'");
+            Debug.WriteLine(webView.LastJSException);
             webView.EvalScript($"{getElementById("login_password")}.value='{password}'");
             Task.Delay(10 * 1000).Wait(token);
             webView.EvalScript($"var a = document.getElementById('loginIFrame').contentDocument.getElementsByClassName('button'); a[0].click()");
-            Debug.WriteLine(webView.LastJSException.ToString());
             Task.Delay(10 * 1000, token).Wait(token);
-            return false;
+            var a =  webView.EvalScript($"{getElementById("emptyFieldErrorContainer")}");
+            Task.Delay(2 * 1000).Wait(token);
+            return a == null;
         }
 
         private string getElementById(string id)
--- ScraperTest/CheckoutBots/FootSites/ChampsSports/ChampsSportsBotTests.cs
+++ ScraperTest/CheckoutBots/FootSites/ChampsSports/ChampsSportsBotTests.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using CheckoutBot.CheckoutBots.FootSites;
 using CheckoutBot.CheckoutBots.FootSites.ChampsSports;
 using CheckoutBot.Core;
 using CheckoutBot.Models;
+using CheckoutBot.Models.Checkout;
 using EO.Wpf;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScraperTest.Helpers;
+using StoreScraper.Bots.Sticky_bit.ChampsSports_EastBay;
 using Helper = ScraperTest.Helpers.Helper;
 using WebView = EO.WebBrowser.WebView;
 
@@ -25,7 +28,29 @@ namespace ScraperTest.CheckoutBots.FootSites.ChampsSports
         [TestMethod()]
         public void AccountCheckoutTest()
         {
-            throw new NotImplementedException();
+            AccountCheckoutSettings settings =
+                new AccountCheckoutSettings()
+                {
+                    UserPassword = "giorgi121",
+                    UserLogin = "gbagh16@freeuni.edu.ge",
+                    UserCcv2 = "123",
+                    ProductToBuy = new FootsitesProduct(new ChampsSportsBot()
+                        , "yle",
+                        "https://www.champssports.com/product/model:283446/sku:A7097514",
+                        0, "", "A7097514")
+                    {
+                        Sku = "A7097514",
+                        Model = "283446",
+                    },
+                    BuyOptions = new ProductBuyOptions()
+                    {
+                        Size = "S"
+                    }
+                };
+            EOBrowserHelper.BotTester(new ChampsSportsBot(){DelayInSecond = 7}, bot =>
+            {
+                bot.AccountCheckout(settings, CancellationToken.None);
+            });
         }
 
         [TestMethod()]
@@ -34,7 +59,9 @@ namespace ScraperTest.CheckoutBots.FootSites.ChampsSports
             EOBrowserHelper.BotTester(new ChampsSportsBot() { DelayInSecond = 7 }, bot =>
             {
                 FootSitesBotBase.Browser.NewTab("loginTab");
-                return bot.Login("gbagh16@freeuni.edu.ge", "giorgi121", CancellationToken.None);
+                bool a = bot.Login("gbagh16@freeuni.edu.ge", "giorgi121", CancellationToken.None);
+                Console.WriteLine(a);
+                return a;
             });
         }
 
